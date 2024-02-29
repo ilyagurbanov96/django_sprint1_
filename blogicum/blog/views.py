80acd1a324eee22a516baff58f0e0aacc406cf41
Эльяр Гурбанов <ilyagurbanov96@gmail.com>
2024-02-29 17:35:58 +0400

    add first commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -46,7 +46,7 @@ posts = [
 
 def index(request):
     template = 'blog/index.html'
-    context = {'index': posts}
+    context = {'index': reversed(posts)}
     return render(request, template, context)
 
 
